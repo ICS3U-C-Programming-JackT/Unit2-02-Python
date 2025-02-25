6eef93c3ad522c8462be631775ec162ef0cbd916
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-25 17:59:59 +0000

    Created two files for constants, and retrieving user input, and displaying circumference

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created By: Jack Turcotte
+# Date: February 25, 2025
+# This code defines the constant Tau for our main script
+
+import math
+
+TAU = math.pi*2;
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Jack Turcotte
+# Date: February 25, 2025
+# This code determines the circumference and area of a circle based on user input
+
+
+import constants
+
+def main():
+    radius = float(input("Enter your radius here (cm): "));
+
+    circumference = radius * constants.TAU;
+
+    print("The circumference of your circle is "+circumference+"cm!")
+
+if __name__ == "__main__":
+    main()
